a8a0cf503c40f148779f25316433d9540da2e37a
VMihail <83347281+VMihail@users.noreply.github.com>
2023-07-25 23:45:06 +0300

    IterativeParallelism final

--- src/main/java/info/kgeorgiy/ja/vasilenko/concurrent/IterativeParallelism.java
+++ src/main/java/info/kgeorgiy/ja/vasilenko/concurrent/IterativeParallelism.java
@@ -1,80 +1,22 @@
 package info.kgeorgiy.ja.vasilenko.concurrent;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class IterativeParallelism implements ScalarIP {
-  /**
-   * Нормализует количетво потоков
-   * @param threadsCount исходное количество потоков
-   * @param collectionLen длинна коллекции
-   * @return {@code Math.min} от обоих аргументов
-   */
-  private int threadCountNormalization(final int threadsCount, final int collectionLen) {
-    return Math.min(threadsCount, collectionLen);
-  }
+/**
+ * impl of {@link ListIP}
+ */
+public class IterativeParallelism implements ListIP {
+  private static final String INTERRUPTED_EXCEPTION_MESSAGE = "Executing thread was interrupted";
 
-  /**
-   * Разделяет список на заданное количество частей
-   * @param values разделяемый список
-   * @param numberOfParts количество частей, на которое мы его делим
-   * @return Список из частей искомого списка
-   * @param <T> тип содержимого списка
-   */
-  private <T> List<List<? extends T>> divide(final List<? extends T> values, int numberOfParts) {
-    numberOfParts = threadCountNormalization(numberOfParts, values.size());
-    final List<List<? extends T>> result = new ArrayList<>(numberOfParts);
-    final int len = values.size() / numberOfParts;
-    for (int i = 0; i < numberOfParts; i++) {
-      final int left = i * len;
-      if (i == numberOfParts - 1) {
-        result.add(values.subList(left, values.size()));
-      } else {
-        result.add(values.subList(left, left + len));
-      }
-    }
-    return result;
-  }
-
-  /**
-   * Выполняет параллельно запрос для списка
-   * @param threads количество потоков
-   * @param values искомый список
-   * @param function функция, которую мы вычисляем для списка
-   * @param functionResult функция, которую мы вычисляет с результатами всех потоков
-   * @return результат запроса
-   * @param <T> тип объектов в списке
-   * @param <R> тип результата запроса
-   * @throws InterruptedException если произошла ошибка с потоками
-   */
-  private <T, R> R doParallel(int threads, final List<? extends T> values,
-                           final Function<Stream<? extends T>, R> function,
-                           final Function<List<R>, R> functionResult) throws InterruptedException {
-    if (values.isEmpty()) {
-      return null;
-    }
-    threads = threadCountNormalization(threads, values.size());
-    final Thread[] threadsArray = new Thread[threads];
-    final List<List<? extends T>> parts = divide(values, threads);
-    final List<R> result = new ArrayList<>(Collections.nCopies(threads, null));
-    for (int i = 0; i < threads; i++) {
-      final int currentInd = i;
-      threadsArray[currentInd] = new Thread(() -> {
-        result.set(currentInd, function.apply(parts.get(currentInd).stream()));
-      });
-      threadsArray[currentInd].start();
-    }
-    for (final var thread : threadsArray) {
-      thread.join();
-    }
-    return functionResult.apply(result);
-  }
   /**
    * Returns maximum value.
    *
@@ -87,9 +29,12 @@ public class IterativeParallelism implements ScalarIP {
    */
   @Override
   public <T> T maximum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
-    return doParallel(threads, values,
+    return executeParallelAction(
+     threads,
+     values,
      (stream -> stream.max(comparator).orElse(null)),
-     (resultList -> resultList.stream().max(comparator).orElse(null)));
+     (stream -> stream.max(comparator).orElse(null))
+    );
   }
 
   /**
@@ -104,9 +49,12 @@ public class IterativeParallelism implements ScalarIP {
    */
   @Override
   public <T> T minimum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
-    return doParallel(threads, values,
+    return executeParallelAction(
+     threads,
+     values,
      (stream -> stream.min(comparator).orElse(null)),
-     (resultList -> resultList.stream().min(comparator).orElse(null)));
+     (stream -> stream.min(comparator).orElse(null))
+    );
   }
 
   /**
@@ -120,9 +68,12 @@ public class IterativeParallelism implements ScalarIP {
    */
   @Override
   public <T> boolean all(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-    return Boolean.TRUE.equals(doParallel(threads, values,
+    return executeParallelAction(
+     threads,
+     values,
      (stream -> stream.allMatch(predicate)),
-     (resultList -> resultList.stream().allMatch(n -> n))));
+     (stream -> stream.allMatch(result -> result))
+    );
   }
 
   /**
@@ -136,7 +87,12 @@ public class IterativeParallelism implements ScalarIP {
    */
   @Override
   public <T> boolean any(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-    return !all(threads, values, predicate);
+    return executeParallelAction(
+     threads,
+     values,
+     (stream -> stream.anyMatch(predicate)),
+     (stream -> stream.anyMatch(result -> result))
+    );
   }
 
   /**
@@ -150,8 +106,146 @@ public class IterativeParallelism implements ScalarIP {
    */
   @Override
   public <T> int count(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-    return doParallel(threads, values,
-     (stream -> (int) stream.filter(predicate).count()),
-     (resultList -> resultList.stream().reduce(Integer::sum).orElse(0)));
+    threads = threadsCountNormalize(threads, values);
+    final var parts = divideIntoParts(values, threads);
+    final List<Integer> results = new ArrayList<>(Collections.nCopies(threads, null));
+    final Thread[] threadArray = new Thread[threads];
+    for (int i = 0; i < threads; i++) {
+      final int index = i;
+      final Thread thread = new Thread(() -> results.set(index, (int) parts.get(index).stream().filter(predicate).count()));
+      threadArray[i] = thread;
+      thread.start();
+    }
+    Arrays.stream(threadArray).forEach((thread -> {
+      try {
+        thread.join();
+      } catch (final InterruptedException e) {
+        throw new RuntimeException(INTERRUPTED_EXCEPTION_MESSAGE, e);
+      }
+    }));
+    return results.stream().reduce(0, Integer::sum);
+  }
+
+  /**
+   * Join values to string.
+   *
+   * @param threads number of concurrent threads.
+   * @param values  values to join.
+   * @return list of joined results of {@link Object#toString()} call on each value.
+   * @throws InterruptedException if executing thread was interrupted.
+   */
+  @Override
+  public String join(int threads, List<?> values) throws InterruptedException {
+    return executeParallelAction(
+     threads,
+     values,
+     stream -> stream.map(Object::toString).collect(Collectors.joining()),
+     stream -> stream.collect(Collectors.joining())
+    );
+  }
+
+  /**
+   * Filters values by predicate.
+   *
+   * @param threads   number of concurrent threads.
+   * @param values    values to filter.
+   * @param predicate filter predicate.
+   * @return list of values satisfying given predicate. Order of values is preserved.
+   * @throws InterruptedException if executing thread was interrupted.
+   */
+  @Override
+  public <T> List<T> filter(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
+    return executeParallelAction(
+     threads,
+     values,
+     stream -> stream.filter(predicate).collect(Collectors.toList()),
+     stream -> stream.flatMap(List::stream).toList()
+    );
+  }
+
+  /**
+   * Maps values.
+   *
+   * @param threads number of concurrent threads.
+   * @param values  values to map.
+   * @param f       mapper function.
+   * @return list of values mapped by given function.
+   * @throws InterruptedException if executing thread was interrupted.
+   */
+  @Override
+  public <T, U> List<U> map(int threads, List<? extends T> values, Function<? super T, ? extends U> f) throws InterruptedException {
+    return executeParallelAction(
+     threads,
+     values,
+     stream -> stream.map(f).collect(Collectors.toList()),
+     stream -> stream.flatMap(List::stream).toList()
+    );
+  }
+
+  /**
+   * Performs a multi-threaded action.
+   * @param threads number of threads
+   * @param values processed List
+   * @param calculateFunction function that each thread executes
+   * @param resultCalculateFunction function to find the overall result
+   * @return result of two functions, type - U
+   * @param <T> type of list elements
+   * @param <U> type of result
+   */
+  private <T, U> U executeParallelAction(
+   int threads,
+   List<? extends T> values,
+   final Function<Stream<? extends T>, ? extends U> calculateFunction,
+   final Function<Stream<? extends U>, ? extends U> resultCalculateFunction
+  ) {
+    threads = threadsCountNormalize(threads, values);
+    final var parts = divideIntoParts(values, threads);
+    final List<U> results = new ArrayList<>(Collections.nCopies(threads, null));
+    final Thread[] threadArray = new Thread[threads];
+    for (int i = 0; i < threads; i++) {
+      final int index = i;
+      final Thread thread = new Thread(() -> results.set(index, calculateFunction.apply(parts.get(index).stream())));
+      threadArray[i] = thread;
+      thread.start();
+    }
+    Arrays.stream(threadArray).forEach((thread -> {
+      try {
+        thread.join();
+      } catch (final InterruptedException e) {
+        throw new RuntimeException(INTERRUPTED_EXCEPTION_MESSAGE, e);
+      }
+    }));
+    return resultCalculateFunction.apply(results.stream());
+  }
+
+  /**
+   * Splits a list into multiple parts.
+   * @param list processed List
+   * @param partsCount number of parts
+   * @return list of sublists
+   * @param <T> type of list elements
+   */
+  private <T> List<List<? extends T>> divideIntoParts(final List<? extends T> list, final int partsCount) {
+    final List<List<? extends T>> result = new ArrayList<>();
+    final int wholePart = list.size() / partsCount;
+    int remains = list.size() % partsCount;
+    int left = 0;
+    for (int i = 0; i < partsCount; i++) {
+      int right = left + wholePart + (remains-- > 0 ? 1 : 0);
+      result.add(list.subList(left, right));
+      left = right;
+    }
+    return result;
+  }
+
+  /**
+   * Normalizes the number of threads.
+   * @param threadsCount number of threads
+   * @param list processed List
+   * @return {@code min(threadsCount, list.size())}
+   * @param <T> type of list elements
+   */
+  private <T> int threadsCountNormalize(final int threadsCount, final List<? extends T> list) {
+    return Math.min(threadsCount, list.size());
   }
 }
